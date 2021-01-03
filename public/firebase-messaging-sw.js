f7e4c341eea5521bdf2ddf55a598e6ba0655af5d
Charif <ayouni_charif@outlook.fr>
2021-01-04 00:31:06 +0100

    Mise en place de Firebase messaging cloud

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,8 +1,27 @@
 // eslint-disable-next-line no-undef
-importScripts('https://www.gstatic.com/firebasejs/8.1.2/firebase-app.js')
+importScripts('./firebasejs/8.1.2/firebase-app.js')
 // eslint-disable-next-line no-undef
-importScripts('https://www.gstatic.com/firebasejs/8.1.2/firebase-messaging.js')
+importScripts('./firebasejs/8.1.2/firebase-messaging.js')
 
 firebase.initializeApp({
-  messagingSenderId: '438866230274'
+  apiKey: 'AIzaSyBFj9s1qZOdjoDNx8mrKESVQe1I5HdrZTw',
+  projectId: 'bike-day',
+  messagingSenderId: '206487854676',
+  appId: '1:206487854676:web:86c25173073c26c6c2d7c1'
+})
+
+const messaging = firebase.messaging()
+messaging.onBackgroundMessage(function (payload) {
+  console.log(
+    '[firebase-messaging-sw.js] Received background message ',
+    payload
+  )
+  // Customize notification here
+  const notificationTitle = payload.notification.title
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: payload.notification.image
+  }
+
+  self.registration.showNotification(notificationTitle, notificationOptions)
 })
